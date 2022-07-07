ca76efeb450805fda9ab69614261692a524d7778
smathot <s.mathot@cogsci.nl>
2022-07-07 11:43:36 +0200

    Bump to 0.3.0 and show version on import with logger.info

--- eeg_eyetracking_parser/__init__.py
+++ eeg_eyetracking_parser/__init__.py
@@ -1,10 +1,12 @@
 """A Python module for reading concurrently recorded EEG and eye-tracking data,
 and parsing this data into convenient objects for further analysis.
 """
-
+import logging
 from ._triggers import trial_trigger, epoch_trigger
 from ._parsing import read_subject
 from ._pupil_epochs import PupilEpochs, epochs_to_series
 
 
-__version__ = '0.2.0'
+__version__ = '0.3.0'
+logger = logging.getLogger('eeg_eyetracking_parser')
+logger.info(f'eeg_eyetracking_parser {__version__}')
