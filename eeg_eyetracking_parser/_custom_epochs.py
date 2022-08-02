ecfc1147748d0b9ee1e4fb3d76635233a5214e23
Hermine <hermine@RN-145-97-170-242.eduroam.rug.nl>
2022-08-02 16:26:25 +0200

    EEG epochs with default autoreject

---
+++ eeg_eyetracking_parser/_custom_epochs.py
@@ -0,0 +1,82 @@
+import mne
+import logging
+import numpy as np
+from datamatrix._datamatrix._seriescolumn import _SeriesColumn
+from datamatrix import series as srs, NAN, operations as ops
+import autoreject as ar
+import os
+
+logger = logging.getLogger('eeg_eyetracking_parser')
+
+
+class PupilEpochs(mne.Epochs):
+    """An Epochs class for the PupilSize channel. This allows baseline
+    correction to be applied to pupil size, even though this channel is not a
+    regular data channel.
+    """
+    
+    def __init__(self, *args, **kwargs):
+        mne.io.pick._PICK_TYPES_DATA_DICT['misc'] = True
+        super().__init__(*args, **kwargs, picks='PupilSize')
+        mne.io.pick._PICK_TYPES_DATA_DICT['misc'] = False
+        
+    def average(self, *args, **kwargs):
+        mne.io.pick._PICK_TYPES_DATA_DICT['misc'] = True
+        evoked = super().average(*args, **kwargs)
+        mne.io.pick._PICK_TYPES_DATA_DICT['misc'] = False
+        return evoked
+
+
+class AutorejectEpochs(mne.Epochs):
+    """An Epochs class for the EEG data where the bad epochs are automatically rejected with autoreject
+    """
+
+    def __init__(self, *args, autoreject_kwargs={}, **kwargs):
+        super().__init__(*args, **kwargs, picks='eeg', preload=True)
+        if 'n_interpolate' not in autoreject_kwargs:
+            autoreject_kwargs['n_interpolate'] = [1, 4, 8, 16]
+        self._autoreject_artifacts(autoreject_kwargs)
+
+    def _autoreject_artifacts(self, autoreject_kwargs):
+        AR = ar.AutoReject(**autoreject_kwargs)
+        AR.fit_transform(self)
+        
+
+def epochs_to_series(dm, epochs, baseline_trim=(-2, 2)):
+    """Takes an Epochs or PupilEpochs object and converts it to a DataMatrix
+    SeriesColumn. If a baseline has been specified in the epoch, it is applied
+    to each row of the series separately. Rows where the mean baseline value
+    (z-scored) is not within the range indicated by `baseline_trim` are set to
+    `NAN`.
+    
+    Parameters
+    ----------
+    dm: DataMatrix
+        A DataMatrix object to which the series belongs
+    epochs: Epochs or PupilEpochs
+        The source object with the epoch data.
+    baseline_trim: tuple of int, optional
+        The range of acceptable baseline values. This refers to z-scores.
+        
+    Returns
+    -------
+    SeriesColumn
+    """
+    a = epochs.get_data()
+    s = _SeriesColumn(dm, depth=a.shape[2])
+    s._seq[epochs.metadata.index] = a.mean(axis=1)
+    if epochs.baseline is not None:
+        start, end = epochs.baseline
+        t = epochs._raw_times
+        i = np.where((t >= start) & (t <= end))[0]
+        bl_start = i[0]
+        bl_end = i[-1]
+        if baseline_trim is not None:
+            bl = srs.reduce(s[:, bl_start : bl_end])
+            for row_nr, (z, bl) in enumerate(zip(ops.z(bl), bl)):
+                if z < baseline_trim[0] or z > baseline_trim[1]:
+                    logging.debug(
+                        f'setting trace to NAN because baseline out of bounds (bl={bl}, z={z})')
+                    s[row_nr] = NAN
+        s = srs.baseline(s, s, bl_start, bl_end)
+    return s
