87cb159b274c9699137973baab0b1d2563185df9
smathot <s.mathot@cogsci.nl>
2022-10-05 16:47:00 +0200

    Bump to 0.9.0

--- eeg_eyetracking_parser/__init__.py
+++ eeg_eyetracking_parser/__init__.py
@@ -4,9 +4,10 @@ and parsing this data into convenient objects for further analysis.
 import logging
 from ._triggers import trial_trigger, epoch_trigger
 from ._parsing import read_subject
-from ._custom_epochs import PupilEpochs, autoreject_epochs, epochs_to_series
+from ._custom_epochs import PupilEpochs, autoreject_epochs, epochs_to_series, \
+    tfr_to_surface
 
 
-__version__ = '0.8.1'
+__version__ = '0.9.0'
 logger = logging.getLogger('eeg_eyetracking_parser')
 logger.info(f'eeg_eyetracking_parser {__version__}')
