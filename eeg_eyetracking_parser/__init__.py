271b70207b40f158da01951a1db4a5f2873f42b8
smathot <s.mathot@cogsci.nl>
2023-08-09 10:41:53 +0200

    Bump to 0.15.4

--- eeg_eyetracking_parser/__init__.py
+++ eeg_eyetracking_parser/__init__.py
@@ -8,6 +8,6 @@ from ._custom_epochs import PupilEpochs, autoreject_epochs, epochs_to_series, \
     tfr_to_surface
 
 
-__version__ = '0.15.3'
+__version__ = '0.15.4'
 logger = logging.getLogger('eeg_eyetracking_parser')
 logger.info(f'eeg_eyetracking_parser {__version__}')
